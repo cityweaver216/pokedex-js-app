b55fe033807d2bb646a2bf06f15c7cb4b0b92f8f
cityweaver216 <57197453+cityweaver216@users.noreply.github.com>
2019-12-23 13:19:07 -0500

    Added pages from computer

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+alert("Hello world");
+var favoritefood = "sushi";
+document.write(favoritefood);
+var starterPKMN = "Bulbasaur";
+var secondPKMN = "Pikachu";
+var thirdPKMN = "Clefable";
+var fourthPKMN = "Gengar";
+var fifthPKMN = "Houndoom";
+var sixthPKMN = "Mewtwo";
+document.write(
+  starterPKMN + secondPKMN + thirdPKMN + fourthPKMN + fifthPKMN + sixthPKMN
+);
