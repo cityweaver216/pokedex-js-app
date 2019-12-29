5ae611319a7ccf3c764d18288f8af29c9c38486c
cityweaver216 <57197453+cityweaver216@users.noreply.github.com>
2019-12-28 23:06:04 -0500

    Update scripts.js with PKMN data
    Added six arrays with Pokemon data for the respository

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,49 @@
-alert("Hello world");
-var favoritefood = "sushi";
-document.write(favoritefood);
-var starterPKMN = "Bulbasaur";
-var secondPKMN = "Pikachu";
-var thirdPKMN = "Clefable";
-var fourthPKMN = "Gengar";
-var fifthPKMN = "Houndoom";
-var sixthPKMN = "Mewtwo";
-document.write(
-  starterPKMN + secondPKMN + thirdPKMN + fourthPKMN + fifthPKMN + sixthPKMN
-);
+var repository = [];
+
+var bulbasaur = {
+  name: "Bulbasaur",
+  height: 0.7,
+  type: ["Grass", "Poison"],
+  attackPhysical: "Body Slam",
+  attackSpecial: "Giga Drain"
+};
+
+var pikachu = {
+  name: "Pikachu",
+  height: 0.4,
+  type: "Electric",
+  attackPhysical: "Quick Attack",
+  attackSpecial: "Thunder"
+};
+
+var clefable = {
+  name: "Clefable",
+  height: 1.3,
+  type: "Fairy",
+  attackPhysical: "Substitute",
+  attackSpecial: "Metronome"
+};
+
+var gengar = {
+  name: "Gengar",
+  height: 1.5,
+  type: ["Ghost", "Poison"],
+  attackPhysical: "Lick",
+  attackSpecial: "Shadow Punch"
+};
+
+var houndoom = {
+  name: "Houndoom",
+  height: 1.4,
+  type: ["Dark", "Fire"],
+  attackPhysical: "Bite",
+  attackSpecial: "Flamethrower"
+};
+
+var mewtwo = {
+  name: "Mewtwo",
+  height: 2,
+  type: "Psychic",
+  attackPhysical: "Swift",
+  attackSpecial: "Psychic"
+};
