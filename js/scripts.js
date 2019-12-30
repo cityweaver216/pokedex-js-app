92a184687d8992135d05c7927ce122284647f57b
cityweaver216 <57197453+cityweaver216@users.noreply.github.com>
2019-12-29 23:38:38 -0500

    Put PKMN into respoitory as objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,46 @@
-var repository = [];
-
-var bulbasaur = {
-  name: "Bulbasaur",
-  height: 0.7,
-  type: ["Grass", "Poison"],
-  attackPhysical: "Body Slam",
-  attackSpecial: "Giga Drain"
-};
-
-var pikachu = {
-  name: "Pikachu",
-  height: 0.4,
-  type: "Electric",
-  attackPhysical: "Quick Attack",
-  attackSpecial: "Thunder"
-};
-
-var clefable = {
-  name: "Clefable",
-  height: 1.3,
-  type: "Fairy",
-  attackPhysical: "Substitute",
-  attackSpecial: "Metronome"
-};
-
-var gengar = {
-  name: "Gengar",
-  height: 1.5,
-  type: ["Ghost", "Poison"],
-  attackPhysical: "Lick",
-  attackSpecial: "Shadow Punch"
-};
-
-var houndoom = {
-  name: "Houndoom",
-  height: 1.4,
-  type: ["Dark", "Fire"],
-  attackPhysical: "Bite",
-  attackSpecial: "Flamethrower"
-};
-
-var mewtwo = {
-  name: "Mewtwo",
-  height: 2,
-  type: "Psychic",
-  attackPhysical: "Swift",
-  attackSpecial: "Psychic"
-};
+var repository = [
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    type: ["Grass", "Poison"],
+    attackPhysical: "Body Slam",
+    attackSpecial: "Giga Drain"
+  },
+  {
+    name: "Pikachu",
+    height: 0.4,
+    type: "Electric",
+    attackPhysical: "Quick Attack",
+    attackSpecial: "Thunder"
+  },
+  {
+    name: "Clefable",
+    height: 1.3,
+    type: "Fairy",
+    attackPhysical: "Substitute",
+    attackSpecial: "Metronome"
+  },
+  {
+    name: "Gengar",
+    height: 1.5,
+    type: ["Ghost", "Poison"],
+    attackPhysical: "Lick",
+    attackSpecial: "Shadow Punch"
+  },
+  {
+    name: "Houndoom",
+    height: 1.4,
+    type: ["Dark", "Fire"],
+    attackPhysical: "Bite",
+    attackSpecial: "Flamethrower"
+  },
+  {
+    name: "Mewtwo",
+    height: 2,
+    type: "Psychic",
+    attackPhysical: "Swift",
+    attackSpecial: "Psychic"
+  }
+];
+console.log(repository[1]);
+console.log(repository[5]);
